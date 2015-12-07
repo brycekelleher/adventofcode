ca946a603cda1814c1b834a76a4f35d3bde1c636
brycek <bryce_kelleher@hotmail.com>
2015-12-08 05:21:51 +1100

    initial solution

--- day6/solution-part1.py
+++ day6/solution-part1.py
@@ -1,25 +1,44 @@
 import sys
 
+state = {}
 
 #function map
 func = dict(zip(['1', '0', 't'], [lambda x: True, lambda x: False, lambda x: not x]))
-def execute(i):
-	state[(x, y)] = func[i[0]](state[(x, y)])
+#def execute(i):
+#	state[(x, y)] = func[i[0]](state[(x, y)])
+
+def rect_test(xy, xy0, xy1):
+	return (int(xy0[0]) <= xy[0] <= int(xy1[0])) and (int(xy0[1]) <= xy[1] <= int(xy1[1]))
+
+def execute_instruction(xy, i):
+	func
+
+def execute(xy, p):
+	for i in p:
+		if not rect_test(xy, i[1], i[2]):
+			continue
+		f = func[i[0]]
+		state[xy] = f(state[xy])
 
 f = sys.stdin
-x = y = 0
-houses = {}
-houses[(x, y)] = True
-for l in f():
-	if c is '>':
-		x += 1
-	if c is '<':
-		x -= 1
-	if c is '^':
-		y += 1
-	if c is 'v':
-		y -= 1
-	houses[(x, y)] = True
+p = []
+for l in f:
+	s =  l.replace(","," ").split()
+	if 'on' in l:
+		p.append( ('1', ( s[2], s[3] ), ( s[5], s[6] ) ) )
+	if 'off' in l:
+		p.append( ('0', ( s[2], s[3] ), ( s[5], s[6] ) ) )
+	if 'toggle' in l: 
+		p.append( ('t', ( s[1], s[2] ), ( s[4], s[5] ) ) )
 
-print len(houses.keys())
+for x in range(500, 750):
+	for y in range(500, 750):
+		state[(x, y)] = False
+		execute((x, y), p)
+count = 0
+for l in state.keys():
+	if state[l] is True:
+		count += 1
 
+print state
+print count
