0647ce8548452329018f654a0c3172457f53f45d
brycek <bryce_kelleher@hotmail.com>
2015-12-23 17:12:54 -0800

    more work on day19

--- day19/solution.py
+++ day19/solution.py
@@ -2,7 +2,7 @@
 
 def read_rules():
 	rules = {}
-	f = open('rules')
+	f = open('testrules')
 	for l in f:
 		s = l.split()
 		i, o = s[0], s[2]
@@ -11,10 +11,49 @@ def read_rules():
 		rules[i].append(o)
 	return rules
 
-def eval_molecule(s, rules):
-	for i in range(len(s)):
-		if s[i:i + 1] in rules
-		if s[i:i + 2]
+def read_molecule():
+	f = open('input')
+	return f.read()
+
+def lookup_rule(s, rules):
+	if s[0:1] in rules:
+		return rules[s[0:1]]
+	elif s[0:2] in rules:
+		return rules[s[0:2]]
+	else:
+		return []
+
+def check_match(s, rules):
+	for i in rules.keys():
+		if s.find(i) == 0:
+			return True, i
+	return False. None
+
+def eval_molecule(m, rules):
+	q = []
+	q.append(("", m))
+	while len(q):
+		print q
+		p, s = q.pop(0)
+
+		# print the completed molecule
+		if s == "":
+			print p
+			continue
+
+		found, key = check_match(s, rules)
+		if not found:
+			q.append((p + s[0], s[1:]))
+			continue
+		rl = rules[key]
+		for r in rl:
+			q.append((p + r, s[len(r):]))
+
 
 rules = read_rules()
+molecule = read_molecule()
+molecule = 'HOH'
 print rules
+print molecule
+
+eval_molecule(molecule, rules)
